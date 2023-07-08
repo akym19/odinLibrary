df10ee0c57dd675e35d73dfec85fa723ed99ce69
Raffy <raffyjay_dayag@yahoo.com>
2023-07-08 23:19:36 +0800

    library layout done. add input section

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, date, read) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.date = date;
     this.read = read;
 }
 
 Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
+    return `${this.title} by ${this.author}, ${this.date}, ${this.read}`
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet')
