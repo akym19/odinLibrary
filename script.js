a69bda91ef5ed01f7b3708fd10b6cbdc874211e1
Raffy <raffyjay_dayag@yahoo.com>
2023-07-09 19:42:48 +0800

    added modal for adding books

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-const myLibrary = [];
+// const myLibrary = [];
 
-function Book(title, author, date, read) {
-    this.title = title;
-    this.author = author;
-    this.date = date;
-    this.read = read;
+// function Book(title, author, date, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.date = date;
+//     this.read = read;
+// }
+
+// Book.prototype.info = function() {
+//     return `${this.title} by ${this.author}, ${this.date}, ${this.read}`
+// }
+
+// const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet')
+// const badLiar = new Book('Bad Liar', 'Imagine Dragons', 1, 'not heard yet')
+
+// const addToLibrary = function (book) {
+//     myLibrary.push(book)
+// }
+
+
+// addToLibrary(theHobbit)
+// addToLibrary(badLiar)
+
+// console.log(myLibrary)
+
+// ----------------- //
+const addBookBtn = document.getElementById('addBook');
+const closeModalBtn = document.getElementById('modalBtn');
+
+let container = document.getElementById('container');
+let modal = document.getElementById('modal')
+
+
+function toggle(){
+    container.classList.toggle('active');
+    modal.classList.toggle('active')
 }
 
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.date}, ${this.read}`
-}
-
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet')
-const badLiar = new Book('Bad Liar', 'Imagine Dragons', 1, 'not heard yet')
-
-const addToLibrary = function (book) {
-    myLibrary.push(book)
-}
-
-
-addToLibrary(theHobbit)
-addToLibrary(badLiar)
-
-console.log(myLibrary)
+addBookBtn.addEventListener('click', toggle)
+closeModalBtn.addEventListener('click', toggle)
